3e487472b4fce02795d2e724788596762ab29810
Blu-Ray <Blu-Ray@DESKTOP-76CE075>
2026-02-25 23:25:25 +0200

    Add project files.

---
+++ College-E-Commerce/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace College_E_Commerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
